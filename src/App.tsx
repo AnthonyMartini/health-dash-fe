47c947036dd2e97c65c8ded14faf9d1a3544d15b
Anthony Martini <anthonycm2022@gmail.com>
2025-01-31 16:27:47 -0500

    added cognito sign in

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@
 
 import React from "react";
 import "./App.css";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 
 const App: React.FC = () => {
+  const { signOut } = useAuthenticator();
   return (
     <div className="container">
       <h1 className="spinning-title rainbow_text_animated">
         AWS Health Dashboard
       </h1>
+      <button onClick={signOut}>Sign out</button>
     </div>
   );
 };
