df8dd33b8c910c038616f5c597967f9f3b95418c
Anthony Martini <anthonycm2022@gmail.com>
2025-01-31 12:35:33 -0500

    Added amplify_outputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,16 @@
-import { useEffect, useState } from "react";
-import type { Schema } from "../amplify/data/resource";
-import { generateClient } from "aws-amplify/data";
+// src/App.tsx
 
-const client = generateClient<Schema>();
+import React from "react";
+import "./App.css";
 
-function App() {
-  const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
-  useEffect(() => {
-    client.models.Todo.observeQuery().subscribe({
-      next: (data) => setTodos([...data.items]),
-    });
-  }, []);
-
-  function createTodo() {
-    client.models.Todo.create({ content: window.prompt("Todo content") });
-  }
-
+const App: React.FC = () => {
   return (
-    <main>
-      <h1>My todos</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
-    </main>
+    <div className="container">
+      <h1 className="spinning-title rainbow_text_animated">
+        AWS Health Dashboard
+      </h1>
+    </div>
   );
-}
+};
 
 export default App;
